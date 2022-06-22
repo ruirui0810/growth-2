21b6b711faad52afeac044b206359fdeac50c418
ruirui0810 <jobonly2525@gmail.com>
2022-06-22 11:25:34 +0900

    テーブル作成

--- db/migrate/20220622022053_create_tweets.rb
+++ db/migrate/20220622022053_create_tweets.rb
@@ -1,7 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string :title
+      t.text :text
+      
       t.timestamps
     end
   end
